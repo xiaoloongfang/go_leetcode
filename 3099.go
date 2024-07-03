1a4448bd1a877eda4193ce08530401085f22cee0
Fang Xiaolong <xiaoloongfang@gmail.com>
2024-07-03 13:32:52 +0800

    daily/0703

--- 3099.go
+++ 3099.go
@@ -1,1 +1,14 @@
 package main
+
+func sumOfTheDigitsOfHarshadNumber(x int) int {
+	now := x
+	sum := 0
+	for now > 0 {
+		sum += now % 10
+		now = now / 10
+	}
+	if x%sum == 0 {
+		return sum
+	}
+	return -1
+}
